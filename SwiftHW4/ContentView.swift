b8c2fa3406cf624856331d588cc834c7bea4ccca
Andrei MARKOV <a981203@mail.ru>
2020-12-22 13:42:16 +0300

    Initial Commit

---
+++ SwiftHW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftHW4
+//
+//  Created by Andrey Markov on 2020-12-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftHW4/SwiftHW4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHW4App.swift
+//  SwiftHW4
+//
+//  Created by Andrey Markov on 2020-12-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
